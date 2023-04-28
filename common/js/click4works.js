3a70f21ab68b9108102827f8df8d08d7e6848904
We-unite <3205135446@qq.com>
2023-04-28 21:53:45 +0800

    Change parameters for yanhua to fit my page

--- common/js/click4works.js
+++ common/js/click4works.js
@@ -8,11 +8,11 @@ window.onclick = (e) => {
             (n.style.top = a + "px"),
             (n.style.background = `radial-gradient(rgb(${255 * Math.random()},${255 * Math.random()},${255 * Math.random()}), transparent)`),
             yanhua.appendChild(n),
-            n.style.setProperty("--t", a + 1e3 * (Math.random() - 0.5) + "px"),
-            n.style.setProperty("--l", t + 1e3 * (Math.random() - 0.5) + "px"),
-            (n.style.animation = "yanhua 2s"),
+            n.style.setProperty("--t", a + 400 * (Math.random() - 0.5) + "px"),
+            n.style.setProperty("--l", t + 400 * (Math.random() - 0.5) + "px"),
+            (n.style.animation = "yanhua 1.5s"),
             setTimeout(() => {
                 n.remove();
-            }, 2e3);
+            }, 1.5e3);
     }
 };
