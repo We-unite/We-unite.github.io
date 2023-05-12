c322b7e3362984b1917f7d8bb4cfd00e8c1de3e6
We-unite <3205135446@qq.com>
2023-05-12 11:20:52 +0800

    Add nation.html & icon

--- common/script4code.js
+++ common/script4code.js
@@ -5,6 +5,7 @@ document.writeln("<head>");
 document.writeln("    <meta charset=\'UTF-8\'>");
 document.writeln("    <meta name=\'viewport\' content=\'width=device-width initial-scale=1\'>");
 document.writeln("    <link rel=\"stylesheet\" href=\"https://www.qin-juan-ge-zhu.top/common/CSS/MySet.css\">");
+document.writeln("    <link rel=\"icon\" type=\"image/x-icon\" href=\"https://www.qin-juan-ge-zhu.top/common/icon.png\" />");
 document.writeln("    <script src=\"https://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js\"></script>");
 document.writeln("    <script type=\"text/javascript\" src=\"https://www.qin-juan-ge-zhu.top/common/js/click4code.js\"></script>");
 document.writeln("</head>");
