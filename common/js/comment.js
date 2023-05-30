bf18e17e2e3b86c6a8b4d0c2766a1437c1a211ff
We-unite <3205135446@qq.com>
2023-05-31 00:18:12 +0800

    Try to fix bugs in comments(giscus)

--- common/js/comment.js
+++ common/js/comment.js
@@ -9,9 +9,9 @@ const scriptElement = document.createElement("script");
 //设置script的属性
 scriptElement.src = "https://giscus.app/client.js";
 scriptElement.setAttribute("data-repo", "We-unite/We-unite.github.io");
-scriptElement.setAttribute("data-repo-id", "R_kgDOHfRjhw");
+scriptElement.setAttribute("data-repo-id", "R_kgDOJU0JLQ");
 scriptElement.setAttribute("data-category", "Announcements");
-scriptElement.setAttribute("data-category-id", "DIC_kwDOHfRjh84CVpjr");
+scriptElement.setAttribute("data-category-id", "DIC_kwDOJU0JLc4CW1vs");
 scriptElement.setAttribute("data-mapping", "title");
 scriptElement.setAttribute("data-strict", "1");
 scriptElement.setAttribute("data-reactions-enabled", "1");
