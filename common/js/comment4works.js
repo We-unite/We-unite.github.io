227b218765e21824bd93173b8f1eb2dbcc2e84cd
We-unite <3205135446@qq.com>
2023-06-01 00:00:38 +0800

    try a new comment in a few works/articles

---
+++ common/js/comment4works.js
@@ -0,0 +1,21 @@
+// 插入一个hr标签，与上下距离各20px
+document.write('<hr style="margin-top: 3em; margin-bottom: 1.5em;">');
+
+// 插入一个<p></p>标签，，居中展示，字体加粗，内容为“一起来讨论吧！\n留下邮箱，接收后续评论喔~”，行距1.5，与下边距离1em
+document.write('<p style="text-align: center; font-weight: bold; line-height: 1.5; margin-bottom: 1em;">一起来讨论吧！<br>留下邮箱，接收后续评论喔~</p>');
+
+// 创建一个div，id为vcomments
+var div = document.createElement('div');
+div.id = 'vcomments';
+
+// 创建一个script标签，内容是一个函数，用于初始化评论框
+var script = document.createElement('script');
+script.innerHTML = `new Valine({
+    el: '#vcomments',
+    appId: 'PuxNi7oFVivphGeGB7ccO5my-gzGzoHsz',
+    appKey: 'K25IlfrYqQ7QuVbG2017h3TC'
+})`;
+//将div写在引用本条script标签之后
+document.write(div.outerHTML);
+//将script添加到上述div之后
+document.write(script.outerHTML);
